cfa611c0a169e6f69beca88efbe8f572eed21cc4
Brian Ngo <b_ngo13@u.pacific.edu>
2024-01-19 16:33:32 -0800

    Changed graddesc to have correct inputs

--- Gradient Descent/GradDesc.m
+++ Gradient Descent/GradDesc.m
@@ -1,9 +1,10 @@
-% Usage: double(GradDesc(f(x), 5, .1, 10))
+% Usage: GradDesc(f(x), 5, .1, 10)
 function res = GradDesc(baseDerivative, inputNum, stepPercent, runTime)
     res = inputNum;
     syms df(x);
     df(x) = diff(baseDerivative);
-    for time = 0:runTime
+    for time = 1:runTime
         res = res - (stepPercent * df(res));
     end
+    res = double(res);
 end
