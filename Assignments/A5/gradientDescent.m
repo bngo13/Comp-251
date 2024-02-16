43576666e84e2c3812f281054898f4d90ad51efa
Brian Ngo <b_ngo13@u.pacific.edu>
2024-02-16 14:00:12 -0800

    Finished part of A5 1-2.1

---
+++ Assignments/A5/gradientDescent.m
@@ -0,0 +1,25 @@
+function theta = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+
+
+
+
+
+
+    % ============================================================
+
+end
+
+end
