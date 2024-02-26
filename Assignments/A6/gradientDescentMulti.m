177905d972064e2aa3e04e72c320a48b3361e374
Brian Ngo <b_ngo13@u.pacific.edu>
2024-02-25 20:24:49 -0800

    Initialize Assignment 6

---
+++ Assignments/A6/gradientDescentMulti.m
@@ -0,0 +1,30 @@
+function theta = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+
+
+
+
+
+
+
+
+
+
+
+    % ============================================================
+
+end
+
+end
