05c3b0322ea0ad856cea899a556b278b0cfd26a3
Brian Ngo <b_ngo13@u.pacific.edu>
2024-02-25 23:07:15 -0800

    AS6 Finish Normalize

--- Assignments/A6/featureNormalize.m
+++ Assignments/A6/featureNormalize.m
@@ -7,8 +7,9 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X);
+sigma = std(X);
+X_norm = (X_norm - mu) ./ sigma;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
